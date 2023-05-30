f177c1c27198e8e29d73ee78e9d3c9fcc24bad4e
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-05-31 01:08:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+function sortArticles(name) {
+	const articles = ['a', 'an', 'the'];
+	return name.replace(/(^a|an|the^)\s/i, '');
+}
+
+touristSpots.sort((a,b) => sortArtilcles(a).localCompare(sortArticles(b)));
+const bandList = document.getElementById("band");
+
+for(let i =0; i<touristSpots.length; i++){
+	var li = document.createElement("li");
+	li.textContent=touristSpots[i];
+	bandList.appendChild(li);
+}
